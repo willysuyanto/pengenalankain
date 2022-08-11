7a155e6e1fa896fb4bc19f555d33cd76ac059108
Pirman Suryadi <pirmansoerjadi@gmail.com>
2022-08-11 17:10:16 +0700

    commit

--- app/src/main/java/com/pirman/pengenalankain/KainAdapter.java
+++ app/src/main/java/com/pirman/pengenalankain/KainAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,14 +21,12 @@ public class KainAdapter extends
     private List<Kain> list;
     private KainAdapterCallback mAdapterCallback;
 
-    public KainAdapter(Context context, List<Kain> list, KainAdapterCallback adapterCallback) {
+        public KainAdapter(KainActivity context, Object kains) {
         this.context = context;
         this.list = list;
-        this.mAdapterCallback = adapterCallback;
+
     }
 
-    public KainAdapter(KainActivity context, Object kains) {
-    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -39,6 +38,12 @@ public class KainAdapter extends
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Kain item = list.get(position);
+        holder.gambar.setText(item.gambar);
+        holder.namaKain.setText(item.namaKain);
+        holder.deskripsi.setText(item.deskripsi);
+        holder.harga.setText((int) item.harga);
+        holder.ketebalan.setText(item.ketebalan);
+        holder.tekstur.setText(item.tekstur);
     }
 
     @Override
@@ -54,8 +59,15 @@ public class KainAdapter extends
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView gambar, namaKain, deskripsi, harga, ketebalan, tekstur;
         public ViewHolder(View itemView) {
             super(itemView);
+            gambar = itemView.findViewById(R.id.content);
+            namaKain = itemView.findViewById(R.id.content);
+            deskripsi = itemView.findViewById(R.id.content);
+            harga = itemView.findViewById(R.id.content);
+            ketebalan = itemView.findViewById(R.id.content);
+            tekstur = itemView.findViewById(R.id.content);
 
         }
     }
